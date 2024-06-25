0affeb730760b3ccae9bb75d5b39d22290c084a2
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-06-24 22:40:02 -0400

    Added print method that returns nothing and has a single number parameter

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 // @ts-check
+//Use a union of literals to declare status options.
+type Status = "started" | "stopped";
 
 class Vehicle {
     //Add appropriate types for all current Vehicle properties and method parameters.
-    status: "started" | "stopped" = "stopped";
+    status: Status = "stopped";
     make: string;
     model: string;
     wheels:number;
@@ -64,13 +66,13 @@ class Vehicle {
 
 class NCycle <T>{
   //Add appropriate types for all current Vehicle properties and method parameters.
-  status: "started" | "stopped" = "stopped";
+  status: Status = "stopped";
  // Allow make and model to have either the generic type or an array of the generic type.
   make: T | T[];
-  model: string;
+  model: T | T[];
   wheels:number;
 
-
+//Adjust the constructor parameters to have either the generic type or an array of the generic type.
   constructor(make:  T | T[], model: T | T[], wheels:number) {
     this.make = make;
     this.model = model;
@@ -82,40 +84,19 @@ class NCycle <T>{
   stop(): void {
     this.status = "stopped";
   }
+
+  //Method print that returns nothing and has a single number parameter 
+  print(x: number = 0) :void{
+    if(!Array.isArray(this.make) && !Array.isArray(this.model)){
+      console.log(`This is a ${this.make} ${this.model} NCycle.`);
+    }else if (Array.isArray(this.make) && Array.isArray(this.model)){
+      console.log(`This NCycle has a ${this.make} ${this.model} NCycle.`)
+    }else{
+      "This NCycle was not created properly."
+    }
+
+
+     // This is a <make> <model> NCycle." if make and model are not arrays.
+
 }
 
-//Adjust the Car according to any TypeScript errors.
-class Car extends Vehicle {
-  constructor(make:string, model:string) {
-    super(make, model, 4);
-  }
-}
-
-//Fixed the MotorCycle classes as needed according to any TypeScript errors.
-class MotorCycle extends Vehicle {
-  constructor(make:string, model:string) {
-    super(make, model, 2);
-  }
-}
-
-//Change the printStatus function to accept a parameter of type Vehicle.
-//Add void becuase function does not return a value
-function printStatus(vehicle: Vehicle): void {
-  if (vehicle.status === "started") {
-    console.log("The vehicle is running.");
-  } else {
-    console.log("The vehicle is stopped.");
-  }
-}
-
-//Case sentitive: toUppercase !== toUpperCase
-const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
-myHarley.start();
-printStatus(myHarley);
-console.log(myHarley.make.toUpperCase());
-
-//Fix mistype of the word model
-const myBuick = new Car("Buick", "Regal");
-myBuick.wheels = myBuick.wheels - 1;
-console.log(myBuick.wheels);
-console.log(myBuick.model);
